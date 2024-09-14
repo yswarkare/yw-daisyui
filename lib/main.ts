971b2d6e7826802dfcc4f3af447a877d30e8aa78
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-09-14 20:55:02 +0530

    kept only dist folder

--- lib/main.ts
+++
@@ -1,6 +0,0 @@
-import InputUi from './ui/InputUi';
-import './index.css';
-import SelectTheme from './theme/select-theme';
-import customThemes from './theme/custom-themes';
-
-export { InputUi, SelectTheme, customThemes };
