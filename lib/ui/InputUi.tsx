ff42d1de07d163af5459fde39c8b8571b0990c51
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-09-15 19:09:23 +0530

    updated input ui

--- lib/ui/InputUi.tsx
+++ lib/ui/InputUi.tsx
@@ -1,34 +1,66 @@
-import { ChangeEventHandler, useState } from "react";
-import { KeyIcon, UserIcon, EmailIcon, SearchIcon } from '../icons/input-ui-icons'
+import { ChangeEventHandler, MouseEvent, MouseEventHandler, useState } from 'react';
+import { KeyIcon, UserIcon, EmailIcon, SearchIcon } from '../icons/input-ui-icons';
 
 const InputIcon = ({ type }: { type: string }) => {
-  switch (type) {
-    case 'search': return <SearchIcon />
-    case 'email': return <EmailIcon />
-    case 'username': return <UserIcon />
-    case 'password': return <KeyIcon />
-    default: return null
-  }
-}
+	switch (type) {
+		case 'search':
+			return <SearchIcon />;
+		case 'email':
+			return <EmailIcon />;
+		case 'username':
+			return <UserIcon />;
+		case 'password':
+			return <KeyIcon />;
+		default:
+			return null;
+	}
+};
 
-const InputUi = ({ type, position, id, onClick, onChange, ...props }: { type: string, position?: string, id?: string, onClick?: any, onChange?: ChangeEventHandler<HTMLInputElement> }) => {
+type Props = {
+	type: string;
+	position?: string;
+	id?: string;
+	onClick?: MouseEventHandler;
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	error?: string;
+};
 
-  const [inType, setInType] = useState(type)
+const InputUi = ({ type, position, id, onClick, onChange, error, ...props }: Props) => {
+	const [inType, setInType] = useState(type);
 
-  const changeIcon = (e: any) => {
-    if (onClick) onClick(e)
-    if (type === 'password') {
-      setInType(inType === 'password' ? 'text' : "password")
-    }
-  }
+	const changeIcon = (e: MouseEvent<HTMLDivElement>) => {
+		if (onClick) onClick(e);
+		if (type === 'password') {
+			setInType(inType === 'password' ? 'text' : 'password');
+		}
+	};
 
-  return (
-    <label className="input input-bordered flex items-center gap-2">
-      {position === 'left' && <div onClick={(e) => changeIcon(e)}><InputIcon type={type} /></div>}
-      <input type={inType === 'password' ? 'password' : "text"} title={id} id={id} className="grow" placeholder={type} onChange={onChange} {...props} />
-      {position === 'right' && <div onClick={(e) => changeIcon(e)}><InputIcon type={type} /></div>}
-    </label>
-  );
-}
+	return (
+		<div className="w-full flex flex-col items-start">
+			<label className="w-full input input-bordered flex items-center gap-2">
+				{position === 'left' && (
+					<div onClick={(e) => changeIcon(e)}>
+						<InputIcon type={type} />
+					</div>
+				)}
+				<input
+					type={inType === 'password' ? 'password' : 'text'}
+					title={id}
+					id={id}
+					className="grow"
+					placeholder={type}
+					onChange={onChange}
+					{...props}
+				/>
+				{position === 'right' && (
+					<div onClick={(e) => changeIcon(e)}>
+						<InputIcon type={type} />
+					</div>
+				)}
+			</label>
+			{error && <p className="text-red-500 text-sm">{error}</p>}
+		</div>
+	);
+};
 
 export default InputUi;
