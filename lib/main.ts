b98523195529c03fca15e2d918901a28af3a5296
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-09-15 14:38:09 +0530

    included icons

--- lib/main.ts
+++ lib/main.ts
@@ -2,5 +2,9 @@ import InputUi from './ui/InputUi';
 import './index.css';
 import SelectTheme from './theme/select-theme';
 import customThemes from './theme/custom-themes';
+import MenuIcon from './icons/menu-icon';
+import DotsIcon from './icons/dots-icon';
 
 export { InputUi, SelectTheme, customThemes };
+
+export { MenuIcon, DotsIcon }
