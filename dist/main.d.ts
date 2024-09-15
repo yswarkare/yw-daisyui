57183a0cc7d6ed3c12d7b1972b8685ef6a7b953d
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-09-15 14:38:20 +0530

    created build

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,4 +1,7 @@
 import { default as InputUi } from './ui/InputUi';
 import { default as SelectTheme } from './theme/select-theme';
 import { default as customThemes } from './theme/custom-themes';
+import { default as MenuIcon } from './icons/menu-icon';
+import { default as DotsIcon } from './icons/dots-icon';
 export { InputUi, SelectTheme, customThemes };
+export { MenuIcon, DotsIcon };
--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,12 @@
-import { default as o } from "./ui/InputUi.js";
+import { default as t } from "./ui/InputUi.js";
 import { default as f } from "./theme/select-theme.js";
-import { default as r } from "./theme/custom-themes.js";
+import { default as m } from "./theme/custom-themes.js";
+import { default as u } from "./icons/menu-icon.js";
+import { default as p } from "./icons/dots-icon.js";
 export {
-  o as InputUi,
+  p as DotsIcon,
+  t as InputUi,
+  u as MenuIcon,
   f as SelectTheme,
-  r as customThemes
+  m as customThemes
 };
