9e1400e9b66b5da421e41c06d9c7e9e11e5b8c38
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2025-01-09 15:13:43 +0530

    updated input component for textarea

--- dist/ui/InputUi.js
+++ dist/ui/InputUi.js
@@ -1,8 +1,8 @@
-import { jsxs as n, jsx as t } from "react/jsx-runtime";
-import { useState as x } from "react";
+import { jsxs as p, jsx as t } from "react/jsx-runtime";
+import { useState as h } from "react";
 import { EyeSlash as w, Eye as I, KeyIcon as N, UserIcon as g, EmailIcon as v, SearchIcon as E } from "../icons/input-ui-icons.js";
-const p = ({ type: s }) => {
-  switch (s) {
+const u = ({ type: a }) => {
+  switch (a) {
     case "search":
       return /* @__PURE__ */ t(E, {});
     case "email":
@@ -14,21 +14,21 @@ const p = ({ type: s }) => {
     default:
       return null;
   }
-}, U = ({ label: s, type: e, position: o, id: i, onClick: c, onChange: d, error: a, ...u }) => {
-  const [r, m] = x(e), f = ["text", "email", "password", "textarea"], h = (l) => {
-    c && c(l), e === "password" && m(r === "password" ? "text" : "password");
+}, j = ({ label: a, type: e, position: c, id: r, onClick: i, onChange: n, error: l, ...d }) => {
+  const [s, m] = h(e), x = ["text", "email", "password", "textarea"], f = (o) => {
+    i && i(o), e === "password" && m(s === "password" ? "text" : "password");
   };
-  return /* @__PURE__ */ n("div", { className: "w-full gap-1 flex flex-col items-start", children: [
-    s && /* @__PURE__ */ t("p", { className: "w-full pl-2 text-left", children: s }),
-    /* @__PURE__ */ n("label", { className: `w-full input input-bordered flex items-center gap-2 ${a && "input-error"}`, children: [
-      o === "left" && /* @__PURE__ */ t("div", { className: "tooltip tooltip-left", "data-tip": s || e, children: /* @__PURE__ */ t(p, { type: e }) }),
-      /* @__PURE__ */ t("input", { type: f.includes(e) ? r : "text", title: i, id: i, className: "grow", placeholder: e, onChange: d, ...u }),
-      e === "password" && /* @__PURE__ */ t("div", { className: "cursor-pointer tooltip tooltip-right", "data-tip": r === "text" ? "hide" : "show", onClick: (l) => h(l), children: r === "text" && /* @__PURE__ */ t(w, {}) || r === "password" && /* @__PURE__ */ t(I, {}) }),
-      o === "right" && /* @__PURE__ */ t("div", { className: "tooltip tooltip-left", "data-tip": s || e, children: /* @__PURE__ */ t(p, { type: e }) })
+  return /* @__PURE__ */ p("div", { className: "w-full gap-1 flex flex-col items-start", children: [
+    a && /* @__PURE__ */ t("p", { className: "w-full pl-2 text-left", children: a }),
+    /* @__PURE__ */ p("label", { className: `w-full input input-bordered flex items-center gap-2 ${l && "input-error"}`, children: [
+      c === "left" && /* @__PURE__ */ t("div", { className: "tooltip tooltip-left", "data-tip": a || e, children: /* @__PURE__ */ t(u, { type: e }) }),
+      e === "textarea" ? /* @__PURE__ */ t("textarea", { title: `${r}_textarea`, id: `${r}_textarea`, className: "grow", placeholder: e, onChange: n, ...d }) : /* @__PURE__ */ t("input", { type: x.includes(e) ? s : "text", title: r, id: r, className: "grow", placeholder: e, onChange: n, ...d }),
+      e === "password" && /* @__PURE__ */ t("div", { className: "cursor-pointer tooltip tooltip-right", "data-tip": s === "text" ? "hide" : "show", onClick: (o) => f(o), children: s === "text" && /* @__PURE__ */ t(w, {}) || s === "password" && /* @__PURE__ */ t(I, {}) }),
+      c === "right" && /* @__PURE__ */ t("div", { className: "tooltip tooltip-left", "data-tip": a || e, children: /* @__PURE__ */ t(u, { type: e }) })
     ] }),
-    a && /* @__PURE__ */ t("p", { className: "w-full pl-2 text-left error text-error text-sm", children: a })
+    l && /* @__PURE__ */ t("p", { className: "w-full pl-2 text-left error text-error text-sm", children: l })
   ] });
 };
 export {
-  U as default
+  j as default
 };
